82e55752dc66f5d18b31753810b38f69d0abfd06
Dimitrios Piliouras <jimpil1985@gmail.com>
2019-12-02 16:57:24 +0000

    adding `secure-random-trick` folder

---
+++ secure-random-trick/test/secure_random_trick/core_test.clj
@@ -0,0 +1,7 @@
+(ns secure-random-trick.core-test
+  (:require [clojure.test :refer :all]
+            [secure-random-trick.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
