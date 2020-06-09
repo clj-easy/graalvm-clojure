604be3ec0876b5eae2c898237694623635e66793
Jeroen van Dijk <jeroentjevandijk@gmail.com>
2020-06-09 13:16:05 +0200

    Touch project.clj file

--- datascript/project.clj
+++ datascript/project.clj
@@ -1,8 +1,7 @@
 (defproject datascript "0.1.0-SNAPSHOT"
 
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [datascript "0.18.10"]
-                 ]
+                 [datascript "0.18.10"]]
 
   :main simple.main
 
