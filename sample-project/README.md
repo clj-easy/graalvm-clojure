ec9f2bb2777df826b5288b133d96e9a45bb2b1c4
Burin Choomnuan <burin.choomnuan@gmail.com>
2021-01-01 16:44:11 -0500

    Update sample-project template
    Use $GRAALVM_HOME variable and update project.clj
    
    - Improve README for the the project

--- sample-project/README.md
+++ sample-project/README.md
@@ -2,6 +2,13 @@
 
 Use this project as template for testing a specific library with GraalVM **native-image**
 
+## Pre-requisite
+
+- Valid installation of GRAALVM 
+- Install `native-image` at least once e.g. `$GRAALVM_HOME/bin/gu native-image`
+- Setup the env for GRAALVM_HOME e.g. `export GRAALVM_HOME=/path/to/your/graalvm-installation`
+- Valid Java installation e.g. `export JAVA_HOME=/path/to/your/java-installation`
+
 ## Usage
 
 Currently testing:
@@ -11,6 +18,5 @@ Currently testing:
 Test with:
 
     lein do clean, uberjar, native, run-native
-
 
 Add any info might be useful for the reader.
--- sample-project/project.clj
+++ sample-project/project.clj
@@ -1,22 +1,29 @@
-(defproject sample-project "0.1.0-SNAPSHOT"
+(def graalvm-home (System/getenv "GRAALVM_HOME"))
 
+(defproject sample-project "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  ;; add the library here
                  ]
-
   :main simple.main
-
   :uberjar-name "simple-main.jar"
-
   :profiles {:uberjar {:aot :all}
              :dev {:plugins [[lein-shell "0.5.0"]]}}
-
   :aliases
-  {"native"
+  {"native-config"
    ["shell"
-    "native-image" "--report-unsupported-elements-at-runtime" "--no-server"
+    ;; run the application to infer the build configuration
+    "${:graalvm-home}/bin/java"
+    "-agentlib:native-image-agent=config-output-dir=./target/config/"
+    "-jar" "./target/${:uberjar-name:-${:name}-${:version}-standalone.jar}"]
+
+   "native"
+   ["shell"
+    "native-image"
+    "--report-unsupported-elements-at-runtime"
+    "--no-server"
     "--initialize-at-build-time"
     "-jar" "./target/${:uberjar-name:-${:name}-${:version}-standalone.jar}"
     "-H:Name=./target/${:name}"]
 
-   "run-native" ["shell" "./target/${:name}"]})
+   "run-native"
+   ["shell" "./target/${:name}"]})
