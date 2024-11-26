0f0a7d3119bfc32138a20872627c01e4d22c6b41
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2024-11-26 19:46:55 -0300

    wip

--- aws-api-s3/project.clj
+++ aws-api-s3/project.clj
@@ -1,30 +1,31 @@
 (defproject aws-api-s3 "0.1.0-SNAPSHOT"
 
-  :dependencies [[org.clojure/clojure         "1.10.2-rc1"]
-                 [com.cognitect.aws/api       "0.8.484"]
-                 [com.cognitect.aws/endpoints "1.1.11.926"]
-                 [com.cognitect.aws/s3        "810.2.817.0"]]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [com.cognitect.aws/api "0.8.692"]
+                 [com.cognitect.aws/endpoints "1.1.12.772"]
+                 [com.cognitect.aws/s3 "869.2.1687.0"]
+                 [com.github.clj-easy/graal-build-time "1.0.5"]]
 
   :main simple.main
 
   :uberjar-name "simple-main.jar"
   :profiles {:uberjar {:aot :all}
-             :dev {:plugins [[lein-shell "0.5.0"]]}}
+             :dev     {:plugins [[lein-shell "0.5.0"]]}}
 
   :aliases
-  {"native-config"
+  {"native"
    ["shell"
-    ;; run the application to infer the build configuration
-    "java" "-agentlib:native-image-agent=config-output-dir=./target/config/"
-    "-jar" "./target/${:uberjar-name:-${:name}-${:version}-standalone.jar}"]
-
-   "native"
-   ["shell"
-    "native-image" "--report-unsupported-elements-at-runtime" "--no-server" "--no-fallback"
-    "-H:+PrintClassInitialization"
-    "-H:ConfigurationFileDirectories=./target/config/"
-    "--initialize-at-build-time"
+    "native-image"
+    "--no-fallback"
+    "--report-unsupported-elements-at-runtime"
+    "--no-server"
     "--allow-incomplete-classpath"
+    "--initialize-at-build-time"
+    "-H:+PrintClassInitialization"
+    "--features=clj_easy.graal_build_time.InitClojureClasses"
+
+    "-H:IncludeResources='.*/service.*edn'"
+
     "--enable-http" "--enable-https" "--enable-all-security-services"
     "-jar" "./target/${:uberjar-name:-${:name}-${:version}-standalone.jar}"
     "-H:Name=./target/${:name}"]
--- aws-api-s3/src/simple/main.clj
+++ aws-api-s3/src/simple/main.clj
@@ -4,7 +4,6 @@
             ;; there are dynamically loaded at runtime
             [cognitect.aws.http.cognitect]
             [cognitect.aws.protocols.json]
-            [cognitect.aws.protocols.common]
             [cognitect.aws.protocols.rest]
             [cognitect.aws.protocols.rest-xml]
             [clojure.spec.alpha])
